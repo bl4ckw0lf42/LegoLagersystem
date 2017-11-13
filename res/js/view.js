2dde56e0afb63541d6a150d4f3dde10fcbe2d3a9
f062 <kevin.krenz@five1.de>
2017-11-13 08:33:05 +0100

    asd

---
+++ res/js/view.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+
+    var $mainDiv = $("#mainStartDiv");
+    var $btnMainConnect = $("#btnMainConnect");
+    var $iconRoboter1 = $("#iconRoboter1");
+    var $iconRoboter2 = $("#iconRoboter2");
+    var $iconRoboter3 = $("#iconRoboter3");
+    var $inputRoboter1 = $("#input_robo_1");
+    var $inputRoboter2 = $("#input_robo_2");
+    var $inputRoboter3 = $("#input_robo_3");
+
+    var $controlLDiv = $("#controllDiv");
+
+
+
+    $btnMainConnect.click(function () {
+        connectRoboter();
+    });
+
+
+    function connectRoboter () {
+        var ipRobo1 = $inputRoboter1.val();
+        var ipRobo2 = $inputRoboter2.val();
+        var ipRobo3 = $inputRoboter3.val();
+
+
+        $mainDiv.css("display", "none");
+        $controlLDiv.css("display", "block");
+        return;
+
+        if (ipRobo1 == "" || ipRobo2 == "" || ipRobo3 == "") return;
+
+        var req1 = createRequest(ipRobo1, "connect");
+        var req2 = createRequest(ipRobo2, "connect");
+        var req3 = createRequest(ipRobo3, "connect");
+
+
+        req.then(function (ev) {
+            console.log("test");
+        });
+
+        req.done(function (ev) {
+            console.log('Yes! Success!');
+        });
+
+        req.catch(function (ev) {
+            console.log('Oh noes!');
+        });
+    }
+
+    function createRequest (url, para) {
+        return $.ajax({
+            url : "http://" + url + "/" + para,
+            dataType : "jsonp",
+            timeout : 10000
+        })
+    }
+});
+
